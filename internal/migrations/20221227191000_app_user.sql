5b2224a64467126ee4d2238240e8412d6a410ad6
Yury <yury.s.nazarov@gmail.com>
2023-01-04 14:56:36 +0300

    [increment_1]: Поправил опечатку

--- internal/migrations/20221227191000_app_user.sql
+++ internal/migrations/20221227191000_app_user.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS app_user (
     id serial 			PRIMARY KEY,
     login 				VARCHAR (255) NOT NULL,
     password 			VARCHAR (255) NOT NULL
-)
+);
 -- +goose StatementEnd
 
 -- +goose Down
